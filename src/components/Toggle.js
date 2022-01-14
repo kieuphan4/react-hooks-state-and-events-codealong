a8aa96037df35a24c97b1c15990a182ce88fbfbd
kieuphan4 <kieuphan530@gmail.com>
2022-01-13 22:21:37 -0600

    commit tree b2a6c85107b8d6f50b88275e6783f9a165e6cf09

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {  useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false); // Set as false because the button should be off when the component first renders
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
